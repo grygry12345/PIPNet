a734548bf3b84f2d4863276148e74034b38db680
Giray Kılıç <kilicgiray@outlook.com>
2023-02-06 20:05:24 +0300

    update test and train sequence

--- run_train.sh
+++ run_train.sh
@@ -1,33 +1,28 @@
 ######################################################################################
 # supervised learning
 
-# 300W, resnet18
-#python lib/train.py experiments/data_300W/pip_32_16_60_r18_l2_l1_10_1_nb10.py
-# 300W, resnet101
-#python lib/train.py experiments/data_300W/pip_32_16_60_r101_l2_l1_10_1_nb10.py
+###################################################################################### mobilenetv3
 
-# COFW, resnet18
-#python lib/train.py experiments/COFW/pip_32_16_60_r18_l2_l1_10_1_nb10.py
-# COFW, resnet101
-#python lib/train.py experiments/COFW/pip_32_16_60_r101_l2_l1_10_1_nb10.py
+# python lib/train.py experiments/data_300W/pip_32_16_60_r50_l2_l1_10_1_nb10_pretrained.py
 
-# WFLW, resnet18
-#python lib/train.py experiments/WFLW/pip_32_16_60_r18_l2_l1_10_1_nb10.py
-# WFLW, resnet101
-#python lib/train.py experiments/WFLW/pip_32_16_60_r101_l2_l1_10_1_nb10.py
+# python lib/train.py experiments/data_300W/pip_32_16_60_r50_l2_l1_10_1_nb10.py
 
-# AFLW, resnet18
-#python lib/train.py experiments/AFLW/pip_32_16_60_r18_l2_l1_10_1_nb10.py
-# AFLW, resnet101
-#python lib/train.py experiments/AFLW/pip_32_16_60_r101_l2_l1_10_1_nb10.py
+###################################################################################### resnet18
 
-######################################################################################
-# GSSL
+# python lib/train.py experiments/data_300W/pip_32_16_60_r18_l2_l1_10_1_nb10_pretrained.py
 
-# 300W + COFW_68 (unlabeled) + WFLW_68 (unlabeled), resnet18, with curriculum
-#python lib/train_gssl.py experiments/data_300W_COFW_WFLW/pip_32_16_60_r18_l2_l1_10_1_nb10_wcc.py 
+# python lib/train.py experiments/data_300W/pip_32_16_60_r18_l2_l1_10_1_nb10.py
 
-# 300W + CelebA (unlabeled), resnet18, with curriculum
-#nohup python lib/train_gssl.py experiments/data_300W_CELEBA/pip_32_16_60_r18_l2_l1_10_1_nb10_wcc.py &
+###################################################################################### resnet50
+
+python lib/train.py experiments/data_300W/pip_32_16_60_r50_l2_l1_10_1_nb10_pretrained.py
+
+python lib/train.py experiments/data_300W/pip_32_16_60_r50_l2_l1_10_1_nb10.py
+
+###################################################################################### resnet101
+
+python lib/train.py experiments/data_300W/pip_32_16_60_r101_l2_l1_10_1_nb10_pretrained.py
+
+python lib/train.py experiments/data_300W/pip_32_16_60_r101_l2_l1_10_1_nb10.py
 
 
