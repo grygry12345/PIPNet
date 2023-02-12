f9c15b9022c09b745813dd3d0bd4e6c33043a21f
Giray Kılıç <kilicgiray@outlook.com>
2023-02-12 20:19:00 +0300

    add script to run demo

--- run_demo.sh
+++ run_demo.sh
@@ -1,11 +1,19 @@
-# image
-python lib/demo.py experiments/WFLW/pip_32_16_60_r18_l2_l1_10_1_nb10.py images/1.jpg
-#python lib/demo.py experiments/data_300W_CELEBA/pip_32_16_60_r18_l2_l1_10_1_nb10_wcc.py images/2.jpg
+# image folder: test_images
 
-# video 
-#python lib/demo_video.py experiments/WFLW/pip_32_16_60_r18_l2_l1_10_1_nb10.py videos/002.avi
-#python lib/demo_video.py experiments/data_300W_CELEBA/pip_32_16_60_r18_l2_l1_10_1_nb10_wcc.py videos/007.avi
+# mobilenetv3
+python lib/demo.py experiments/data_300W/pip_32_16_60_mobilenetv3_l2_l1_10_1_nb10_pretrained.py test_images
+python lib/demo.py experiments/data_300W/pip_32_16_60_mobilenetv3_l2_l1_10_1_nb10.py test_images
 
-# camera 
-#python lib/demo_video.py experiments/WFLW/pip_32_16_60_r18_l2_l1_10_1_nb10.py camera
+# resnet18
+python lib/demo.py experiments/data_300W/pip_32_16_60_r18_l2_l1_10_1_nb10_pretrained.py test_images
+python lib/demo.py experiments/data_300W/pip_32_16_60_r18_l2_l1_10_1_nb10.py test_images
+
+# resnet50
+python lib/demo.py experiments/data_300W/pip_32_16_60_r50_l2_l1_10_1_nb10_pretrained.py test_images
+python lib/demo.py experiments/data_300W/pip_32_16_60_r50_l2_l1_10_1_nb10.py test_images
+
+# resnet101
+python lib/demo.py experiments/data_300W/pip_32_16_60_r101_l2_l1_10_1_nb10_pretrained.py test_images
+python lib/demo.py experiments/data_300W/pip_32_16_60_r101_l2_l1_10_1_nb10.py test_images
+
 
